249eb9bcc135ceff04b2858aaed86fa6b39c6b5a
Faisal Zaghloul <faisal.zaghloul@gmail.com>
2023-12-08 19:49:17 -0500

    rotation compelling enough

--- src/game/keyboard_movement_controller.cpp
+++ src/game/keyboard_movement_controller.cpp
@@ -7,6 +7,7 @@ void KeyboardMovementController::moveInPlaneXZ(
     GLFWwindow* window, float dt, LveGameObject& gameObject
 ) {
   glm::vec3 rotate{0};
+
   if (glfwGetKey(window, keys.lookRight) == GLFW_PRESS) rotate.y += 1.f;
   if (glfwGetKey(window, keys.lookLeft) == GLFW_PRESS) rotate.y -= 1.f;
   if (glfwGetKey(window, keys.lookUp) == GLFW_PRESS) rotate.x += 1.f;
@@ -17,11 +18,14 @@ void KeyboardMovementController::moveInPlaneXZ(
   }
 
   float yaw = gameObject.transform.rotation.y;
-  const glm::vec3 forwardDir{sin(yaw), 0.f, cos(yaw)};
-  const glm::vec3 rightDir{forwardDir.z, 0.f, -forwardDir.x};
+  // const glm::vec3 forwardDir{sin(yaw), 0.f, cos(yaw)};
+  // const glm::vec3 rightDir{forwardDir.z, 0.f, -forwardDir.x};
+  const glm::vec3 forwardDir{0, 0.f, -1.f};
+  const glm::vec3 rightDir{-forwardDir.z, 0.f, forwardDir.x};
   const glm::vec3 upDir{0.f, 1.f, 0.f};
 
   glm::vec3 moveDir{0.f};
+
   if (glfwGetKey(window, keys.moveForward) == GLFW_PRESS) moveDir += forwardDir;
   if (glfwGetKey(window, keys.moveBackward) == GLFW_PRESS) moveDir -= forwardDir;
   if (glfwGetKey(window, keys.moveRight) == GLFW_PRESS) moveDir += rightDir;
