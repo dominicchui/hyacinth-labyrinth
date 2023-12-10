b0c00c82cee2c7a55cff4927d42654b311ed7856
Dominic Chui <dominic.chui@outlook.com>
2023-12-10 14:52:58 -0500

    Add method to increase maze connectivity

--- src/maze/cell.cpp
+++ src/maze/cell.cpp
@@ -8,17 +8,17 @@ Cell::Cell()
 
 std::string Cell::toString() {
     if (type == CellType::Open) {
-        // temp debug
-        switch (exitDir) {
-        case Direction::N:
-            return "N";
-        case Direction::E:
-            return "E";
-        case Direction::S:
-            return "S";
-        case Direction::W:
-            return "W";
-        }
+//        // temp debug
+//        switch (exitDir) {
+//        case Direction::N:
+//            return "N";
+//        case Direction::E:
+//            return "E";
+//        case Direction::S:
+//            return "S";
+//        case Direction::W:
+//            return "W";
+//        }
 //        return "O";
         return {PATH_REPRESENTATION};
     } else if (type == CellType::Closed) {
