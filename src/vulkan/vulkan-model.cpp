0436ec345fb144236b0c19f3f16529ed2d44562c
Faisal Zaghloul <faisal.zaghloul@gmail.com>
2023-12-11 23:02:53 -0500

    sampler created

--- src/vulkan/vulkan-model.cpp
+++ src/vulkan/vulkan-model.cpp
@@ -45,11 +45,13 @@ VKModel::VKModel(
 {
     createTextureImage();
     createTextureImageView();
+    createTextureSampler();
     createVertexBuffers(builder.vertices);
     createIndexBuffers(builder.indices);
 }
 
 VKModel::~VKModel() {
+    vkDestroySampler(m_device.device(), textureSampler, nullptr);
     vkDestroyImageView(m_device.device(), textureImageView, nullptr);
     vkDestroyImage(m_device.device(), textureImage, nullptr);
     vkFreeMemory(m_device.device(), textureImageMemory, nullptr);
@@ -140,6 +142,36 @@ void VKModel::createTextureImage(void) {
                                    VK_FORMAT_R8G8B8A8_SRGB,
                                    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
                                    VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+}
+
+void VKModel::createTextureSampler(void) {
+    VkSamplerCreateInfo samplerInfo{};
+    samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    samplerInfo.magFilter = VK_FILTER_LINEAR;
+    samplerInfo.minFilter = VK_FILTER_LINEAR;
+
+    samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+
+    samplerInfo.anisotropyEnable = VK_TRUE;
+    samplerInfo.maxAnisotropy = m_device.properties.limits.maxSamplerAnisotropy;
+
+    samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+
+    samplerInfo.unnormalizedCoordinates = VK_FALSE;
+
+    samplerInfo.compareEnable = VK_FALSE;
+    samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+
+    samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    samplerInfo.mipLodBias = 0.0f;
+    samplerInfo.minLod = 0.0f;
+    samplerInfo.maxLod = 0.0f;
+
+    if (vkCreateSampler(m_device.device(), &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create texture sampler!");
+    }
 }
 
 void VKModel::createTextureImageView() {
